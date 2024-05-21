fc743dcfc42c0ee7a2f1a53043defd52c7e3b3a2
unknown <aminuemmazon@gmaill.com>
2024-05-21 20:09:26 +0100

    React App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import AboutUs from "./pages/static/AboutUs";
+import Contact from "./pages/static/Contact";
+import CreateUser from "./pages/user/CreateUser";
+import EditUser from "./pages/user/EditUser";
+import RemoveUser from "./pages/user/RemoveUser";
+import RetrieveUser from "./pages/user/RetrieveUser";
+import UsersList from "./pages/user/UsersList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.min.css";
 
-
-export default() => {
+const App = () => {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UsersList />} />
+          <Route path="/create" element={<CreateUser />} />
+          <Route path="/edit/:userId" element={<EditUser />} />
+          <Route path="/remove/:userId" element={<RemoveUser />} />
+          <Route path="/:userId" element={<RetrieveUser />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
-
+export default App;
